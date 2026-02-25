051a948970c10c8f8f20b193ac31da256427a4c0
Harsh77-prog <kumar345hk@gmail.com>
2026-02-25 20:21:44 +0530

    update

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  experimental: {
+    outputFileTracingIncludes: {
+      '/**/*': ['./node_modules/.prisma/client/**/*'],
+    },
+  },
 };
 
 export default nextConfig;
